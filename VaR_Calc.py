aff0c97890bf6dcae053fafc75cfd428a101ea7c
Cameron Beesley <59356485+funnyfriend77@users.noreply.github.com>
2026-05-06 09:28:20 -0600

    Did the same using JPY

--- VaR_Calc.py
+++ VaR_Calc.py
@@ -7,8 +7,6 @@ data = yf.download(ticker, start="2021-01-01", end="2026-01-01")['Close']
 # Calculate daily percentage change
 returns = data.pct_change().dropna()
 
-print(returns)
-
 import numpy as np
 
 confidence_level = 0.95
@@ -20,6 +18,28 @@ var_amount = portfolio_value * var_percentile
 
 print(f"95% 1-day VaR: £{abs(var_amount):,.2f}")
 
+# create a histogram showing the distribution of returns
+import matplotlib.pyplot as plt
+plt.hist(returns, bins=100)
+plt.show()
+
+# ----------------------------------------- Fetch EUR/JPY ---------------------------------------------------
+ticker = "EURJPY=X"
+data = yf.download(ticker, start="2021-01-01", end="2026-01-01")['Close']
+
+# Calculate daily percentage change
+returns = data.pct_change().dropna()
+
+confidence_level = 0.95
+# Find the 5th percentile (the bottom 5% of days)
+var_percentile = np.percentile(returns, (1 - confidence_level) * 100)
+
+portfolio_value = 10_000_000  # £10 Million
+var_amount = portfolio_value * var_percentile
+
+print(f"95% 1-day VaR: £{abs(var_amount):,.2f}")
+
+# create a histogram showing the distribution of returns
 import matplotlib.pyplot as plt
 plt.hist(returns, bins=100)
 plt.show()

361688e18e0b75cfedce54dcdad76ab33e9afa60
Cameron Beesley <59356485+funnyfriend77@users.noreply.github.com>
2026-05-07 06:02:49 -0600

    Added some more tickers to the correlation matrix

--- VaR_Calc.py
+++ VaR_Calc.py
@@ -2,46 +2,46 @@ import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Fetch EUR/USD data
-ticker_USD = "EURUSD=X"
-data_USD = yf.download(ticker_USD, start="2021-01-01", end="2026-01-01")['Close']
+# # Fetch EUR/USD data
+# ticker_USD = "EURUSD=X"
+# data_USD = yf.download(ticker_USD, start="2021-01-01", end="2026-01-01")['Close']
 
-# Calculate daily percentage change
-returns_USD = data_USD.pct_change().dropna()
+# # Calculate daily percentage change
+# returns_USD = data_USD.pct_change().dropna()
 
-confidence_level_USD = 0.95
-# Find the 5th percentile (the bottom 5% of days)
-var_percentile_USD = np.percentile(returns_USD, (1 - confidence_level_USD) * 100)
+# confidence_level_USD = 0.95
+# # Find the 5th percentile (the bottom 5% of days)
+# var_percentile_USD = np.percentile(returns_USD, (1 - confidence_level_USD) * 100)
 
-portfolio_value_USD = 10_000_000  # £10 Million
-var_amount_USD = portfolio_value_USD * var_percentile_USD
+# portfolio_value_USD = 10_000_000  # £10 Million
+# var_amount_USD = portfolio_value_USD * var_percentile_USD
 
-print(f"95% 1-day VaR: £{abs(var_amount_USD):,.2f}")
+# print(f"95% 1-day VaR: £{abs(var_amount_USD):,.2f}")
 
-# create a histogram showing the distribution of returns
-plt.hist(returns_USD, bins=100)
-plt.show()
+# # create a histogram showing the distribution of returns
+# plt.hist(returns_USD, bins=100)
+# plt.show()
 
-# ----------------------------------------- Fetch EUR/JPY ---------------------------------------------------
-ticker_JPY = "EURJPY=X"
-data_JPY = yf.download(ticker_JPY, start="2021-01-01", end="2026-01-01")['Close']
+# # ----------------------------------------- Fetch EUR/JPY ---------------------------------------------------
+# ticker_JPY = "EURJPY=X"
+# data_JPY = yf.download(ticker_JPY, start="2021-01-01", end="2026-01-01")['Close']
 
-# Calculate daily percentage change
-returns_JPY = data_JPY.pct_change().dropna()
+# # Calculate daily percentage change
+# returns_JPY = data_JPY.pct_change().dropna()
 
-confidence_level_JPY = 0.95
-# Find the 5th percentile (the bottom 5% of days)
-var_percentile_JPY = np.percentile(returns_JPY, (1 - confidence_level_JPY) * 100)
+# confidence_level_JPY = 0.95
+# # Find the 5th percentile (the bottom 5% of days)
+# var_percentile_JPY = np.percentile(returns_JPY, (1 - confidence_level_JPY) * 100)
 
-portfolio_value_JPY = 10_000_000  # £10 Million
-var_amount_JPY = portfolio_value_JPY * var_percentile_JPY
+# portfolio_value_JPY = 10_000_000  # £10 Million
+# var_amount_JPY = portfolio_value_JPY * var_percentile_JPY
 
-print(f"95% 1-day VaR: £{abs(var_amount_JPY):,.2f}")
+# print(f"95% 1-day VaR: £{abs(var_amount_JPY):,.2f}")
 
-# create a histogram showing the distribution of returns
-import matplotlib.pyplot as plt
-plt.hist(returns_JPY, bins=100)
-plt.show()
+# # create a histogram showing the distribution of returns
+# import matplotlib.pyplot as plt
+# plt.hist(returns_JPY, bins=100)
+# plt.show()
 
 #----------------------------------------- Creating a correlation martix---------------------------------------------------
 
@@ -49,7 +49,7 @@ import pandas as pd
 import seaborn as sns
 
 # Getting data and returns
-tickers_MATRIX = ["EURUSD=X", "EURJPY=X"]
+tickers_MATRIX = ["EURUSD=X", "EURJPY=X", "GBPUSD=X", "GC=F", "^GSPC", "^TNX"] 
 data_MATRIX = yf.download(tickers_MATRIX, start="2021-01-01", end="2026-01-01")['Close']
 returns_MATRIX = data_MATRIX.pct_change().dropna()
 
